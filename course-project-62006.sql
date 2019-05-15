fc081b018ffce5539ce06175a2f533df248d2e37
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-16 00:14:32 +0300

    Add files via upload

--- course-project-62006.sql
+++ course-project-62006.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS course_project_62006.subtitle;
-DROP TABLE IF EXISTS course_project_62006.image;
-DROP TABLE IF EXISTS course_project_62006.video;
-DROP TABLE IF EXISTS course_project_62006.user;
-DROP TABLE IF EXISTS course_project_62006.role;
-DROP DATABASE IF EXISTS course_project_62006;
+DROP TABLE IF EXISTS video_annotation.subtitle;
+DROP TABLE IF EXISTS video_annotation.image;
+DROP TABLE IF EXISTS video_annotation.video;
+DROP TABLE IF EXISTS video_annotation.user;
+DROP TABLE IF EXISTS video_annotation.role;
+DROP DATABASE IF EXISTS video_annotation;
 
-CREATE DATABASE course_project_62006 CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE video_annotation CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE course_project_62006.role (
+CREATE TABLE video_annotation.role (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL UNIQUE,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE course_project_62006.user (
+CREATE TABLE video_annotation.user (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(100) CHARACTER SET utf8 NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE course_project_62006.user (
 	FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE course_project_62006.video(
+CREATE TABLE video_annotation.video(
 	id INT NOT NULL AUTO_INCREMENT,
 	path VARCHAR(510) NOT NULL,
 	name VARCHAR(255) NOT NULL UNIQUE,
@@ -34,7 +34,7 @@ CREATE TABLE course_project_62006.video(
 	FOREIGN KEY (user_id) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE course_project_62006.image(
+CREATE TABLE video_annotation.image(
 	id INT NOT NULL AUTO_INCREMENT,
 	path VARCHAR(510) NOT NULL,
 	name VARCHAR(255) NOT NULL UNIQUE,
@@ -46,7 +46,7 @@ CREATE TABLE course_project_62006.image(
 	FOREIGN KEY (video_id) REFERENCES video(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE course_project_62006.subtitle(
+CREATE TABLE video_annotation.subtitle(
 	id INT NOT NULL AUTO_INCREMENT,
 	path VARCHAR(510) NOT NULL,
 	name VARCHAR(255) NOT NULL UNIQUE,
@@ -56,9 +56,9 @@ CREATE TABLE course_project_62006.subtitle(
 	FOREIGN KEY (video_id) REFERENCES video(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-INSERT INTO course_project_62006.role(name) VALUES('Admin');
-INSERT INTO course_project_62006.role(name) VALUES('User');
-INSERT INTO course_project_62006.user(email, name, surname, password, role_id) VALUES('zdravko.gyurov97@gmail.com', N'Здравко', N'Гюров', '$2y$10$LLyr9O/5n9.MJtGYMCltUerYIO8pbfzWYBJxT9EbCnIqF84PIJqLa', '1');
-INSERT INTO course_project_62006.video(path, name, type, user_id) VALUES('video_path', 'video_name', 'video_type', '1');
-INSERT INTO course_project_62006.image(path, name, type, timestamp, annotation, video_id) VALUES('image_path', 'image_name', 'image_type', '60', 'image_annotation', '1');
-INSERT INTO course_project_62006.subtitle(path, name, type, video_id) VALUES('subtitle_path', 'subtitle_name', 'subtitle_type', '1');
+INSERT INTO video_annotation.role(name) VALUES('Admin');
+INSERT INTO video_annotation.role(name) VALUES('User');
+INSERT INTO video_annotation.user(email, name, surname, password, role_id) VALUES('zdravko.gyurov97@gmail.com', N'Здравко', N'Гюров', '$2y$10$LLyr9O/5n9.MJtGYMCltUerYIO8pbfzWYBJxT9EbCnIqF84PIJqLa', '1');
+INSERT INTO video_annotation.video(path, name, type, user_id) VALUES('video_path', 'video_name', 'video_type', '1');
+INSERT INTO video_annotation.image(path, name, type, timestamp, annotation, video_id) VALUES('image_path', 'image_name', 'image_type', '60', 'image_annotation', '1');
+INSERT INTO video_annotation.subtitle(path, name, type, video_id) VALUES('subtitle_path', 'subtitle_name', 'subtitle_type', '1');
