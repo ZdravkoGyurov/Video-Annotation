2a370f4246c0af882308275ec8922ae231bfe48f
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-17 20:57:47 +0300

    Add files via upload

--- api/api.php
+++ api/api.php
@@ -1,19 +1,24 @@
 <?php
     include_once '../config/Database.php';
+
     include_once '../models/User.php';
     include_once '../models/Video.php';
     include_once '../models/Image.php';
     include_once '../models/Subtitle.php';
+
     include_once 'user/create.php';
     include_once 'user/login.php';
     include_once 'user/logout.php';
     include_once 'user/update.php';
     include_once 'user/delete.php';
+    include_once 'user/findAll.php';
 
     $database = new Database();
     $connection = $database->getConnection();
 
     $uriSegments = explode('.php/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+    $errors = array();
 
     if($uriSegments[1] == 'register') {
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -35,15 +40,27 @@
             $user = new User($connection);
             updatePassword($user);
         }
+    } else if($uriSegments[1] == 'all-users') {
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $user = new User($connection);
+            findAll($user);
+        }
     } else if(strpos($uriSegments[1], '/')) {
         $segments = explode('/', $uriSegments[1]);
         if($segments[0] == 'delete-user') {
             if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
                 $user = new User($connection);
-                $email = $segments[1];
-                deleteUser($user, $email);
+                deleteUser($user, $segments[1]);
             }
+        } else {
+            $errors['pageNotFoundError'] = 'Page not found!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
         }
     } else {
-        echo 'Page not found.';
+        $errors['pageNotFoundError'] = 'Page not found!';
+        echo json_encode(array(
+            'errors' => $errors
+        ));
     }
--- api/user/findAll.php
+++ api/user/findAll.php
@@ -1,1 +1,35 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    function findAll($user) {
+        $allUsers = $user->findAllusers();
+        $num = $allUsers->rowCount();
+
+        if(isset($_COOKIE['loggedUserEmail']) && !empty(isset($_COOKIE['loggedUserEmail']))) {
+            $errors = array();
+
+            $user->findUserByEmail($_COOKIE['loggedUserEmail']);
+
+            if($user->roleName == 'Admin') {
+                if($num > 0) {
+                    echo json_encode($allUsers->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+                } else {
+                    $errors['noUserFoundError'] = 'No users found!';
+                    echo json_encode(array(
+                        'errors' => $errors
+                    ));
+                }
+            } else {
+                $errors['unauthorizedUserError'] = 'You are unauthorized!';
+                echo json_encode(array(
+                    'errors' => $errors
+                ));
+            }
+        } else {
+            $errors['noAdminError'] = 'You need to be logged in as Admin to see all registered users!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
+        }
+    }
