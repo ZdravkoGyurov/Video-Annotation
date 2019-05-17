41a1071d4ce335a13bad5d7248da16d6f90ed63e
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-17 12:12:08 +0300

    Add files via upload

--- api/user/create.php
+++ api/user/create.php
@@ -1,1 +1,47 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: '.
+    'Access-Control-Allow-Headers, Content-Type, '.
+    'Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    function register($user) {
+        $data = json_decode(file_get_contents('php://input'));
+
+        if($data->email != '' && $data->name != '' && $data->surname != '' && $data->password != '' && $data->passwordRepeat != '') {
+            $errors = array();
+            // validate fields
+            // validate email
+            // validate name
+            // validate surname
+            // validate password
+            // validate passwordRepeat
+
+            if(empty($errors)) {
+                define("email", $data->email);
+                define("name", $data->name);
+                define("surname", $data->surname);
+                define("password", password_hash($data->password, PASSWORD_DEFAULT));
+                define("roleUser", 2);
+
+                if($user->createUser(email, name, surname, password, roleUser)) {
+                    echo json_encode($user, JSON_UNESCAPED_UNICODE);
+                } else {
+                    $errors['userExistsError'] = 'That email address has already been used!';
+                    echo json_encode(array(
+                        'errors' => $errors
+                    ));
+                }
+            } else {
+                echo json_encode(array(
+                    'errors' => $errors
+                ));
+            }
+        } else {
+            $errors['dataIncompleteError'] = 'Register failed! Data is incomplete!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
+        }
+    }
