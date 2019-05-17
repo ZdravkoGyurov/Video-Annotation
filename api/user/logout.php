acab2b3a1e51d13c3f607a13e178f97a203b2b38
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-17 19:56:38 +0300

    Add files via upload

--- api/user/logout.php
+++ api/user/logout.php
@@ -1,1 +1,28 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    function logout($user) {
+        if(isset($_COOKIE['loginInfo']) && !empty(isset($_COOKIE['loginInfo']))) {
+            $errors = array();
+
+            $user->findUserByEmail($_COOKIE['loginInfo']);
+
+            if($user->roleName == 'Admin' || $user->roleName == 'User') {
+                setcookie('loggedUserEmail', '', time() - 3600, '/');
+                setcookie('loggedUserInfo', '', time() - 3600, '/');
+
+                echo json_encode($user, JSON_UNESCAPED_UNICODE);
+            } else {
+                $errors['unauthorizedUserError'] = 'Cannot log out! User is unauthorized!';
+                echo json_encode(array(
+                    'errors' => $errors
+                ));
+            }
+        } else {
+            $errors['loginError'] = 'No user is logged in!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
+        }
+    }
