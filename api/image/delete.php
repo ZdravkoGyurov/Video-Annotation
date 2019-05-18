8f615d74a39af548b56121fd8c2bd27843a09492
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-18 14:01:26 +0300

    Add files via upload

---
+++ api/image/delete.php
@@ -0,0 +1,62 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: DELETE');
+    header('Access-Control-Allow-Headers: '.
+    'Access-Control-Allow-Headers, Content-Type, '.
+    'Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    function deleteImage($user, $video, $image) {
+        $data = json_decode(file_get_contents('php://input'));
+
+        if(isset($data->videoId) && isset($data->timestamp)) {
+            if(isset($_COOKIE['loggedUserEmail']) && !empty(isset($_COOKIE['loggedUserEmail']))) {
+                $errors = array();
+
+                $user->findUserByEmail($_COOKIE['loggedUserEmail']);
+
+                if($user->roleName == 'User') {
+                    // validate videoId
+                    // validate timestamp
+
+                    if(empty($errors)) {
+                        $video->findVideoById($data->videoId);
+                        $image->findImageByVideoNameAndTimestamp($video->name, $data->timestamp);
+
+                        if(!isset($image->name)) {
+                            $errors['noSuchImageFoundError'] = 'No image with given video id and timestamp found!';
+                            echo json_encode(array(
+                                'errors' => $errors
+                            ));
+                        } else if($image->deleteImage($data->videoId, $data->timestamp)) {
+                            echo json_encode($image, JSON_UNESCAPED_UNICODE);
+                        } else {
+                            $errors['unableToDeleteImageError'] = 'Image could not be deleted!';
+                            echo json_encode(array(
+                                'errors' => $errors
+                            ));
+                        }
+                    } else {                
+                    echo json_encode(array(
+                        'errors' => $errors
+                    ));
+                    }
+                } else {
+                    $errors['unauthorizedUserError'] = 'You are unauthorized!';
+                    echo json_encode(array(
+                        'errors' => $errors
+                    ));
+                }
+            } else {
+                $errors['noAdminError'] = 'You need to be logged in as User to delete images!';
+                echo json_encode(array(
+                    'errors' => $errors
+                ));
+            }
+        } else {
+            $errors['dataIncompleteError'] = 'Image could not be deleted! Data is incomplete!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
+        }
+    }
