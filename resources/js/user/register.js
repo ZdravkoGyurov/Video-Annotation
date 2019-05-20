77d5e90494fcd088e2e310060d952dc7c573b86e
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-20 19:53:59 +0300

    Add files via upload

---
+++ resources/js/user/register.js
@@ -0,0 +1,31 @@
+var form = document.getElementById("registerForm");
+
+form.addEventListener("submit", submitRegisterForm);
+
+function submitRegisterForm() {
+    event.preventDefault();
+
+    var email = document.getElementById('email').value;
+    var name = document.getElementById('name').value;;
+    var surname = document.getElementById('surname').value;;
+    var password = document.getElementById('password').value;
+    var passwordRepeat = document.getElementById('passwordRepeat').value;
+    var formData = JSON.stringify({email:email, name:name, surname:surname, password:password, passwordRepeat:passwordRepeat});
+
+    // validate fields
+
+    if(true) { // no errors
+        $.ajax({
+            type: "POST",
+            url: "../../api/api.php/register",
+            dataType: "json",
+            data: formData,
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(response) {
+                console.log(response);
+            }
+        });
+    }
+};
