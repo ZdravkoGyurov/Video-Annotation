acc298ddec16392fabeb5360bf5908b8396c56b2
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-22 00:08:44 +0300

    Add files via upload

--- resources/js/user/all-users.js
+++ resources/js/user/all-users.js
@@ -9,10 +9,31 @@ function showAllUsers() {
         type: "GET",
         url: "../../api/api.php/all-users",
         success: function(response) {
-            console.log(response);
+            var list = document.getElementById('list-all-users');
+            for(key in response.data) {
+                var value = response.data[key];
+
+                var item = document.createElement('li');
+                item.appendChild(document.createTextNode(value.id + " " + value.email + " " + value.name + " " + value.surname + " " + value.roleName));
+                list.appendChild(item);
+
+                var btn = document.createElement('button');
+                    btn.id = "btn-" + value.id;
+                    btn.innerHTML = "Delete";
+                    btn.addEventListener("click", function(e)  {
+                        if(e.target && e.target.id.startsWith("btn-")) {
+                            log(e.target.id.replace('btn-',''));
+                        }
+                    });
+                list.appendChild(btn);
+            }
         },
         error: function(response) {
             console.log(response);
         }
     });
 };
+
+function log(num) {
+    console.log(num);
+}
