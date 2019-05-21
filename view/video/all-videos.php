177ff7e0de4d23c34f9131785f6cb7863dd69c7a
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-21 21:57:02 +0300

    Add files via upload

---
+++ view/video/all-videos.php
@@ -0,0 +1,7 @@
+<?php $pageTitle = "VA | Restore password" ?>
+<?php include '../common/header.html' ?>
+<link rel="stylesheet" href="../../resources/css/all-videos.css">
+<h1>All videos</h1>
+
+<script type="text/javascript" src="../../resources/js/video/all-videos.js"></script>
+<?php include '../common/footer.html' ?>
