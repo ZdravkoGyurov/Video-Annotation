8de996d6c2fbf5b1abd5b65fc0723dd075abb557
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-22 00:14:28 +0300

    Add files via upload

--- view/user/all-users.php
+++ view/user/all-users.php
@@ -2,6 +2,7 @@
 <?php include '../common/header.html' ?>
 <link rel="stylesheet" href="../../resources/css/all-users.css">
 <h1>All users</h1>
-
+<ul id="list-all-users">
+</ul>
 <script type="text/javascript" src="../../resources/js/user/all-users.js"></script>
 <?php include '../common/footer.html' ?>
