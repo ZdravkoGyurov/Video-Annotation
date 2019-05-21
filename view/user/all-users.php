b2e4f1a9a1491e0aab9cab1620158cdd88294a7b
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-21 22:29:10 +0300

    Add files via upload

---
+++ view/user/all-users.php
@@ -0,0 +1,7 @@
+<?php $pageTitle = "VA | All users" ?>
+<?php include '../common/header.html' ?>
+<link rel="stylesheet" href="../../resources/css/all-users.css">
+<h1>All users</h1>
+
+<script type="text/javascript" src="../../resources/js/user/all-users.js"></script>
+<?php include '../common/footer.html' ?>
