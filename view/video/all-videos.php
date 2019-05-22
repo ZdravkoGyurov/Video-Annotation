918332cea66b8a8b9428c2685c6d472438685305
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-22 12:44:28 +0300

    Add files via upload

--- view/video/all-videos.php
+++ view/video/all-videos.php
@@ -2,6 +2,7 @@
 <?php include '../common/header.html' ?>
 <link rel="stylesheet" href="../../resources/css/all-videos.css">
 <h1>All videos</h1>
-
+<ul id="list-all-videos">
+</ul>
 <script type="text/javascript" src="../../resources/js/video/all-videos.js"></script>
 <?php include '../common/footer.html' ?>
--- view/video/your-videos.php
+++ view/video/your-videos.php
@@ -2,6 +2,7 @@
 <?php include '../common/header.html' ?>
 <link rel="stylesheet" href="../../resources/css/your-videos.css">
 <h1>Your videos</h1>
-
+<ul id="list-all-videos">
+</ul>
 <script type="text/javascript" src="../../resources/js/video/your-videos.js"></script>
 <?php include '../common/footer.html' ?>
