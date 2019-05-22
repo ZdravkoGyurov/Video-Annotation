6b09137c4f83a92af7cf89ca9df26ce7fb83b5e7
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-22 19:12:48 +0300

    Add files via upload

---
+++ view/video/upload-video.php
@@ -0,0 +1,15 @@
+<?php $pageTitle = "VA | Upload video" ?>
+<?php include '../common/header.html' ?>
+<link rel="stylesheet" href="../../resources/css/upload-video.css">
+<h1>Upload video</h1>
+<form enctype="multipart/form-data" id="uploadFileForm" >
+    <input id="hidden-input" name="uploaded-file" accept="video/*" type="file" hidden="hidden">
+    <div id="whole-button">
+        <i class="fas fa-upload"></i>
+        <button id="btn" type="button">Choose a File</button>
+    </div>
+    <p id="message">No file chosen</p>
+    <button id="btn-upload" type="submit">Upload</button>
+</form>
+<script type="text/javascript" src="../../resources/js/video/upload-video.js"></script>
+<?php include '../common/footer.html' ?>
