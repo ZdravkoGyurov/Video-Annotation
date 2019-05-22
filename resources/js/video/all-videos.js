39cf70254e8c7311d4e607634281303ef1ca3d81
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-22 12:45:13 +0300

    Add files via upload

--- resources/js/video/all-videos.js
+++ resources/js/video/all-videos.js
@@ -9,7 +9,48 @@ function showAllVideos() {
         type: "GET",
         url: "../../api/api.php/all-videos",
         success: function(response) {
+            var list = document.getElementById('list-all-videos');
+            for(key in response.data) {
+                var value = response.data[key];
+
+                var item = document.createElement('li');
+                item.appendChild(document.createTextNode(value.name));
+                list.appendChild(item);
+
+                var decodedCookieRole = decodeURIComponent(getCookie('loggedUserRole'));
+
+                if(decodedCookieRole == "Admin") {
+                    var btn = document.createElement('button');
+                    btn.id = "btn-" + value.id;
+                    btn.innerHTML = "Delete";
+                    btn.setAttribute("name", value.name)
+                    btn.addEventListener("click", function(e)  {
+                        if(e.target && e.target.id.startsWith("btn-")) {
+                            removeVideo(e.target.getAttribute("name"));
+                        }
+                    });
+                    item.appendChild(btn);
+                }
+            }
             console.log(response);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
+};
+
+function removeVideo(name) {
+    event.preventDefault();
+
+    var url = "../../api/api.php/delete-video/" + name;
+
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        success: function(response) {
+            console.log(response);
+            location.reload();
         },
         error: function(response) {
             console.log(response);
--- resources/js/video/your-videos.js
+++ resources/js/video/your-videos.js
@@ -22,7 +22,44 @@ function showYourVideos() {
         type: "GET",
         url: url,
         success: function(response) {
+            var list = document.getElementById('list-all-videos');
+            for(key in response.data) {
+                var value = response.data[key];
+
+                var item = document.createElement('li');
+                item.appendChild(document.createTextNode(value.name));
+                list.appendChild(item);
+
+                var btn = document.createElement('button');
+                btn.id = "btn-" + value.id;
+                btn.innerHTML = "Delete";
+                btn.setAttribute("name", value.name)
+                btn.addEventListener("click", function(e)  {
+                    if(e.target && e.target.id.startsWith("btn-")) {
+                        removeVideo(e.target.getAttribute("name"));
+                    }
+                });
+                item.appendChild(btn);
+            }
             console.log(response);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
+};
+
+function removeVideo(name) {
+    event.preventDefault();
+    alert(name);
+    var url = "../../api/api.php/delete-video/" + name;
+
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        success: function(response) {
+            console.log(response);
+            location.reload();
         },
         error: function(response) {
             console.log(response);
