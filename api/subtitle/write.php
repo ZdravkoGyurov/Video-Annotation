436f06fc2f7c744fbb32df5dda132e4b6126bab7
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-25 21:37:51 +0300

    Add files via upload

---
+++ api/subtitle/write.php
@@ -0,0 +1,54 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: '.
+    'Access-Control-Allow-Headers, Content-Type, '.
+    'Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    function writeSubtitle($user, $video, $subtitle) {
+        $data = json_decode(file_get_contents('php://input'));
+
+        if($data->startTime != '' && $data->endTime != '' && $data->annotation != '' && $data->videoId != '' && $data->videoUserId != '') {
+            if(isset($_COOKIE['loggedUserEmail']) && !empty(isset($_COOKIE['loggedUserEmail']))) {
+                $errors = array();
+
+                // validate fields
+                $user->findUserByEmail($_COOKIE['loggedUserEmail']);
+    
+                if($user->roleName == 'User' && $user->id == $data->videoUserId) {
+                    if(empty($errors)) {
+
+                        $subtitle->findSubtitleByVideoId($data->videoId);
+
+                        $filePath = $subtitle->path;
+                        $currentContent = file_get_contents($filePath);
+                        $currentContent .= "\n".$data->startTime."-->".$data->endTime."\n".$data->annotation."\n";
+                        file_put_contents($filePath, $currentContent);
+
+                        echo json_encode($subtitle, JSON_UNESCAPED_UNICODE);
+                    } else {                
+                        echo json_encode(array(
+                            'errors' => $errors
+                        ));
+                    }
+                } else {
+                    $errors['unauthorizedUserError'] = 'You are unauthorized!';
+                    echo json_encode(array(
+                        'errors' => $errors
+                    ));
+                }
+            } else {
+                $errors['noUserError'] = 'You need to be logged as User to upload images!';
+                echo json_encode(array(
+                    'errors' => $errors
+                ));
+            }
+        } else {
+            $errors['dataIncompleteError'] = 'Cannot find image! Data is incomplete!';
+            echo json_encode(array(
+                'errors' => $errors
+            ));
+        }
+
+    }
