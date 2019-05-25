2f5df68bb1adcd29583735651b9d267bbf9f2817
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-05-25 11:22:53 +0300

    Add files via upload

--- api/video/find.php
+++ api/video/find.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     
-    function findVideo($video, $name) {
+    function findVideo($video, $image, $subtitle, $name) {
         if(isset($_COOKIE['loggedUserEmail']) && !empty(isset($_COOKIE['loggedUserEmail']))) {
             $errors = array();
 
@@ -10,14 +10,38 @@
 
             if(empty($errors)) {
                 $video->findVideoByName($name);
+                $subtitle->findSubtitleByVideoId($video->id);
+                $allImages = $image->findAllImagesByVideoName($name);
+                $numberOfImages = $allImages->rowCount();
+                $imagesArr = array();
+                $imagesArr['data'] = array();
 
+                if($numberOfImages > 0) {
+                    while($row = $allImages->fetch(PDO::FETCH_ASSOC)) {
+                        $imageItem = array(
+                            'id' => $row['id'],
+                            'path' => $row['path'],
+                            'name' => $row['name'],
+                            'type' => $row['type'],
+                            'timestamp' => $row['timestamp'],
+                            'annotation' => $row['annotation'],
+                            'videoId' => $row['video_id'],
+                            'videoName' => $row['video_name']
+                        );
+                        array_push($imagesArr['data'], $imageItem);
+                    }
+                }
                 if(!isset($video->name)) {
                     $errors['noSuchVideoFoundError'] = 'No video with given id found!';
                     echo json_encode(array(
                         'errors' => $errors
                     ));
                 } else {
-                    echo json_encode($video, JSON_UNESCAPED_UNICODE);
+                    echo json_encode(array(
+                        'video' => $video,
+                        'images' => $imagesArr,
+                        'subtitle' => $subtitle
+                    ), JSON_UNESCAPED_UNICODE);
                 }
             } else {             
                 echo json_encode(array(
