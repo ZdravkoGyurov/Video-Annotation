7fca912be2c5f47955460bff2296854174dd8c4d
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-06-02 20:30:14 +0300

    Add files via upload

--- resources/js/user/login.js
+++ resources/js/user/login.js
@@ -5,13 +5,18 @@ form.addEventListener("submit", submitLoginForm);
 function submitLoginForm() {
     event.preventDefault();
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var formData = JSON.stringify({email:email, password:password});
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
 
-    // validate fields
+    validateEmail(email, emailError);
+    validatePassword(password, passwordError);
+    
+    if(!emailError.innerHTML && !passwordError.innerHTML) {
+        var formData = JSON.stringify({email:email.value, password:password.value});
 
-    if(true) { // no errors
         $.ajax({
             type: "POST",
             url: "../../api/api.php/login",
@@ -19,14 +24,18 @@ function submitLoginForm() {
             data: formData,
             success: function(response) {
                 if(response.errors) {
-                    console.log(response.errors);
+                    var generalError = document.getElementById('generalError');
+                    generalError.style.display = "block";
+                    generalError.innerText = "";
+                    for(key in response.errors) {
+                        generalError.innerText += response.errors[key] + "\n";
+                    }
                 } else {
                     location.replace("../video/all-videos.php");
                 }
             },
-            error: function(response) {
-                location.reload(true);
-                console.log(response);
+            error: function() {
+                alert("CONNECTION ERROR");
             }
         });
     }
@@ -41,12 +50,35 @@ document.addEventListener("DOMContentLoaded", function(){
     if(register) {
         var successMessage = document.createElement('p');
         successMessage.id = "success-message";
-        successMessage.innerHTML = "Registration successful!";
+        successMessage.innerHTML = "Registration successful";
         document.getElementsByTagName("h1")[0].prepend(successMessage);
     } else if(restore) {
         var successMessage = document.createElement('p');
         successMessage.id = "success-message";
-        successMessage.innerHTML = "Password restoration successful!";
+        successMessage.innerHTML = "Password restoration successful";
         document.getElementsByTagName("h1")[0].prepend(successMessage);
     }
 });
+
+function displayErrorField(field, fieldError, errorMessage) {
+    fieldError.innerHTML = errorMessage;
+    fieldError.style.display = "block";
+    field.value = "";
+}
+
+function validateEmail(email, emailError) {
+    var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!pattern.test(email.value)) {
+        displayErrorField(email, emailError, "Email is invalid");
+    } else {
+        emailError.innerHTML = "";
+    }
+}
+
+function validatePassword(password, passwordError) {
+    if(password.value.length <= 0) {
+        displayErrorField(password, passwordError, "Password is too short");
+    } else {
+        passwordError.innerHTML = "";
+    }
+}
