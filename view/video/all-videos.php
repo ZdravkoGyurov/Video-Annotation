2911b1234d1dca984cf1c9d482632c71960d8659
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-07-03 19:07:41 +0300

    Add files via upload

--- view/video/all-videos.php
+++ view/video/all-videos.php
@@ -7,6 +7,7 @@
 <?php include '../common/header.html'; ?>
 <link rel="stylesheet" href="../../resources/css/all-videos.css">
 <h1>All videos</h1>
+<p id="generalError" class="error"></p>
 <ul id="list-all-videos">
 </ul>
 <script type="text/javascript" src="../../resources/js/video/all-videos.js"></script>
