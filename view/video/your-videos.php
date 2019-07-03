a38d1f9a0fa88c6cef9191faf9ab18a9fc73cfd4
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-07-03 20:27:29 +0300

    Add files via upload

--- view/video/your-videos.php
+++ view/video/your-videos.php
@@ -11,6 +11,7 @@
 <?php include '../common/header.html'; ?>
 <link rel="stylesheet" href="../../resources/css/your-videos.css">
 <h1>Your videos</h1>
+<p id="generalError" class="error"></p>
 <ul id="list-all-videos">
 </ul>
 <script type="text/javascript" src="../../resources/js/video/your-videos.js"></script>
